538a2d1f4ea88b0e1586026e569fc84f229da9c1
RizkiMufrizal <6929062+RizkiMufrizal@users.noreply.github.com>
2021-02-22 13:44:33 +0700

    update test

--- src/test/java/org/rizki/mufrizal/async/BelajarAsyncApplicationTests.java
+++ src/test/java/org/rizki/mufrizal/async/BelajarAsyncApplicationTests.java
@@ -1,13 +1,49 @@
 package org.rizki.mufrizal.async;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.rizki.mufrizal.async.httpclient.AsyncHttpClient;
+import org.rizki.mufrizal.async.httpclient.SyncHttpClient;
+import org.rizki.mufrizal.async.mapper.AlbumMapper;
+import org.rizki.mufrizal.async.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 @SpringBootTest
+@Slf4j
 class BelajarAsyncApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private AsyncHttpClient asyncHttpClient;
+
+    @Autowired
+    private SyncHttpClient syncHttpClient;
+
+    @Test
+    void asyncHttpClientTest() throws ExecutionException, InterruptedException, JsonProcessingException {
+        log.info("Start Test Async Test");
+        CompletableFuture<AlbumMapper> albumMapperResponseMapperCompletableFuture = asyncHttpClient.getAlbum();
+        CompletableFuture<CategoryMapper> categoryMapperCompletableFuture = asyncHttpClient.getCategory();
+
+        log.info("Result Get Album {}", new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(albumMapperResponseMapperCompletableFuture.get()));
+        log.info("Result Get Category {}", new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(categoryMapperCompletableFuture.get()));
+        log.info("End Test Async Test");
+    }
+
+    @Test
+    void syncHttpClientTest() throws ExecutionException, InterruptedException, JsonProcessingException {
+        log.info("Start Test Sync Test");
+        CompletableFuture<AlbumMapper> albumMapperResponseMapperCompletableFuture = syncHttpClient.getAlbum();
+        CompletableFuture<CategoryMapper> categoryMapperCompletableFuture = syncHttpClient.getCategory();
+
+        log.info("Result Get Album {}", new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(albumMapperResponseMapperCompletableFuture.get()));
+        log.info("Result Get Category {}", new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(categoryMapperCompletableFuture.get()));
+        log.info("End Test Sync Test");
+    }
 
 }
