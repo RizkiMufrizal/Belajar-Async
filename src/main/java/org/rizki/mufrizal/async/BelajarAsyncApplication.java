a6b0364bf467ec59bb467977329eb9f177b066ca
RizkiMufrizal <6929062+RizkiMufrizal@users.noreply.github.com>
2021-02-22 13:25:19 +0700

    initial project

---
+++ src/main/java/org/rizki/mufrizal/async/BelajarAsyncApplication.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarAsyncApplication.class, args);
+	}
+
+}
